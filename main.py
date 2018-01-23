8b121d32d5e9b9b71bb9c21a43c9defad484979c
Joseph Sayegh <sayegh_j@hotmail.com>
2018-01-23 18:13:17 +0100

    created the first basic tree

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from criteria import Criteria
 from nodetype import NodeType
 from instructions import Assign, If, Skip, While
 from condition import Condition, BooleanOperator
+from arithmetic_expression import Add, Minus, Time
 
 label = label
 state = {} # X : 0
@@ -18,24 +19,24 @@ tests = [] # liste de tests
 analyse_couverture(prog, [Criteria.TA], tests)
 
 Variables = {'X': None}
-v1 = (1, Variables, If('X'))
-v2 = (2, Variables, Skip)
-v3 = (3, Variables, Skip)
-v4 = (4, Variables, If('X'))
-v5 = (5, Variables, Skip)
-v6 = (6, Variables, Skip)
-v7 = ('_', Variables, Skip)
+v1 = (1, Variables, NodeType.IF)
+v2 = (2, Variables, NodeType.NONE)
+v3 = (3, Variables, NodeType.NONE)
+v4 = (4, Variables, NodeType.IF)
+v5 = (5, Variables, NodeType.NONE)
+v6 = (6, Variables, NodeType.NONE)
+v7 = ('_', Variables, NodeType.NONE)
 
 V = [v1, v2, v3, v4, v5, v6, v7]
 
-e1 = (v1, v2, Condition('X', 0, "<="), Skip())
-e2 = (v1, v3, Condition(Condition('X', 0, "<="), operator = "NOT"), Skip())
-e3 = (v2, v4, Condition(True), Assign('X', '-X'))
-e4 = (v3, v4, Condition(True), Assign('X', '1-X'))
-e5 = (v4, v5, Condition('X', 1, "="), Skip())
-e6 = (v4, v5, Condition(Condition('X', 1, "="), operator = "NOT"), Skip())
-e7 = (v5, v7, Condition(True), Assign('X', '1'))
-e8 = (v6, v7, Condition(True), Assign('X', 'X + 1'))
+e1 = (v1, v2, Condition(Variables['X'], 0, "<="), Skip())
+e2 = (v1, v3, Condition(Condition(Variables['X'], 0, "<="), operator = "NOT"), Skip())
+e3 = (v2, v4, Condition(True), Assign(Variables['X'], Minus(0,Variables['X'])))
+e4 = (v3, v4, Condition(True), Assign(Variables['X'], Minus(1, Variables['X'])))
+e5 = (v4, v5, Condition(Variables['X'], 1, "="), Skip())
+e6 = (v4, v5, Condition(Condition(Variables['X'], 1, "="), operator = "NOT"), Skip())
+e7 = (v5, v7, Condition(True), Assign(Variables['X'], 1))
+e8 = (v6, v7, Condition(True), Assign(Variables['X'], Add(Variables['X'], 1)))
 
 E = [e1, e2, e3, e4, e5, e6, e7, e8]
 
