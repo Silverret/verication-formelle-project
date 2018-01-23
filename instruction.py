fe6ebcf6f8b5cc1dce7bf0c6bdba432f2997b024
Silvestre Perret <silvestre.perret@gmail.com>
2018-01-23 15:53:22 +0100

    add instruction.py

---
+++ instruction.py
@@ -0,0 +1,15 @@
+class Instruction:
+    """
+    None : Not IF nor While
+    """
+    NONE = 0
+
+    """
+    If : IF node
+    """
+    IF = 1
+
+    """
+    While : WHILE node
+    """
+    WHILE = 2
